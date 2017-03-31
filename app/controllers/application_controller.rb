1873daee2e70705f6f2438a7d006acb70d6b6c66
Jonas <jonjts@gmail.com>
2017-03-31 10:07:12 -0300

    Melhoria na identificação da lingua padrão do browser.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action do
-  	I18n.locale = params[:locale] || I18n.default_locale
+  	I18n.locale = params[:locale] || extract_locale_from_accept_language_header
   end
 
   def default_url_options
 	{ locale: I18n.locale }
   end
+
+  def extract_locale_from_accept_language_header
+  case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    when 'en'
+      'en'
+    when 'pt'
+      'pt-BR'
+    when 'pt-BR'
+      'pt-BR'
+    else
+      'en'
+  end
+end
 
 end

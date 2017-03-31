bca7476b44ca2900c8f45115dfb8bc0093f205ab
Jonas <jonjts@gmail.com>
2017-03-31 12:46:40 -0300

    Teste de thema mobile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  scope "(:locale)", locale: /en|pt/ do
+  scope "(:locale)", locale: /en|pt-BR/ do
   	
   end
 
-  get '/:locale' => 'home#index', locale: /en|pt/
+  get '/:locale' => 'home#index', locale: /en|pt-BR/
   get '/auth/spotify/callback', to: 'home#spotify',  locale: /en|pt/
   root :to => "home#index"
 end
